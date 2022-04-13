8a7bb235ee138b52b4e1415fe80364c43020c3fb
Jichan Choi <pnlkc@naver.com>
2022-04-13 21:09:36 +0900

    2.0

--- app/src/main/java/com/example/examplerecyclerview/ItemList.kt
+++ app/src/main/java/com/example/examplerecyclerview/ItemList.kt
@@ -3,10 +3,13 @@ package com.example.examplerecyclerview
 object ItemList {
 
     val itemList = arrayListOf<ItemData>(
-        ItemData("저는 크롬이예요!", R.mipmap.ic_launcher),
-        ItemData("저는 인터넷 익스플로러예요!", R.mipmap.ic_launcher),
-        ItemData("저는 웨일이예요!", R.mipmap.ic_launcher),
-        ItemData("저는 엣지예요!", R.mipmap.ic_launcher),
-        ItemData("저는 파이어 폭스예요!", R.mipmap.ic_launcher),
+        ItemData("저는 애플이예요!", R.drawable.apple),
+        ItemData("저는 크롬이예요!", R.drawable.chrome),
+        ItemData("저는 페북이예요!", R.drawable.facebook),
+        ItemData("저는 인스타예요!", R.drawable.instagram),
+        ItemData("저는 레딧이예요!", R.drawable.reddit),
+        ItemData("저는 트위터예요!", R.drawable.twitter),
+        ItemData("저는 윈도우예요!", R.drawable.windows),
+        ItemData("저는 유튜브예요!", R.drawable.youtube)
     )
 }
