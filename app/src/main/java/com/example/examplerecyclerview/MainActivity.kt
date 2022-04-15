1b151b3c28faa3dce0a79f7319e4bf8ed02ed57c
Jichan Choi <pnlkc@naver.com>
2022-04-15 22:14:30 +0900

    2.1

--- app/src/main/java/com/example/examplerecyclerview/MainActivity.kt
+++ app/src/main/java/com/example/examplerecyclerview/MainActivity.kt
@@ -36,5 +36,9 @@ class MainActivity : AppCompatActivity() {
 
         // activity_main의 리사이클러뷰 레이아웃 메니져를 리니어레이아웃으로 설정하는 코드
         binding.recyclerView.layoutManager = LinearLayoutManager(this)
+
+        // 위의 레이아웃 매니져 설정을 xml 파일에서
+        // app:layoutManager="androidx.recyclerview.widget.LinearLayoutManager"
+        // 처럼 설정해도 같은 기능임
     }
 }
